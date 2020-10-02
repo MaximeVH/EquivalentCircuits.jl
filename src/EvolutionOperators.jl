03a712711eec622f809038040776dd6e8e070f67
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2020-10-02 10:04:16 +0200

    adjust evolutionary operators

--- src/EvolutionOperators.jl
+++ src/EvolutionOperators.jl
@@ -8,7 +8,7 @@ of p2. The output is a new LRC_Circuit object.
 function one_point_crossover(circuit1,circuit2)
     karva1 = circuit1.karva
     karva2 = circuit2.karva
-    crossover_point = rand(1:length(karva1)-1)
+    crossover_point = rand(1:(length(karva1)-1))
     new_karva = karva1[1:crossover_point]*karva2[crossover_point+1:end]
     new_parameters =  vcat(circuit1.parameters[1:crossover_point],circuit2.parameters[crossover_point+1:end])
     ET,inds = Karva_to_ET(new_karva)
@@ -20,7 +20,7 @@ end
     two_point_crossover(circuit1,circuit2)
 
 Recombine two parent circuits, p1 and p2, by replacing a randomly selected
-component or subcircuit of p1 by a randomly selected subcircuit (that can also be nothing)
+component or subcircuit of p1 by a randomly selected subciiiiircuit (that can also be nothing)
 of p2. The output is a new LRC_Circuit object.
 """
 function two_point_crossover(circuit1,circuit2)
@@ -40,7 +40,7 @@ A given circuit p1 is subjected to mutation with a probability of mutation_proba
 mutation entails the generation of a new component/subcircuit that is randomly inserted in the
 parent circuit.
 """
-function mutate(circuit,terminals = "LRC")
+function mutate(circuit,terminals = "LRCP")
     #You could adjust later so that only operations can be replaced in first position.
     karva = circuit.karva
     parameters = circuit.parameters
@@ -57,11 +57,19 @@ function mutate(circuit,terminals = "LRC")
     if position > head_length
         new_terminal_element = rand(terminals)
         mutable_karva[position] = new_terminal_element
-        parameters[position] = rand()*Dict('R'=>1000,'C'=> 0.001,'L'=> 1)[new_terminal_element]
+        if new_terminal_element != 'P'
+            parameters[position] = rand()*Dict('R'=>1000,'C'=> 0.001,'L'=> 1)[new_terminal_element]
+        else
+            parameters[position] = [rand()*100,rand()*1]
+        end
     elseif position <= head_length
         new_head_element = rand(operations*terminals)
         mutable_karva[position] = new_head_element
-        parameters[position] = rand()*Dict('R'=>1000,'C'=> 0.001,'L'=> 1,'+'=> 0,'-'=> 0)[new_head_element]
+        if new_head_element != 'P'
+            parameters[position] = rand()*Dict('R'=>1000,'C'=> 0.001,'L'=> 1,'+'=> 0,'-'=> 0)[new_head_element]
+        else
+            parameters[position] = [rand()*100,rand()*1]
+        end
     end
     new_karva = String(mutable_karva)
     # new_circuit = karva_to_circuit(new_karva)
@@ -94,11 +102,11 @@ function circuit_offspring(circuit_1,circuit_2)
     return mutate(offspring)
 end
 
-function CircuitFitness(circuit,measurements,frequencies)
-    CircFunc = CircuitFunction(circuit.circuit)
+function CircuitFitness_inclusive(circuit,measurements,frequencies)
+    CircFunc = CircuitFunction_inclusive_Final(circuit.circuit)
     ObjFunc = ObjectiveFunction(CircFunc,measurements,frequencies)
-    OptParams = OptimizeParameters(ObjFunc,circuit.parameters[circuit.parameter_indices])
-    return OptParams, ObjFunc(OptParams)
+    OptParams = OptimizeParameters(ObjFunc,to_flat_array(circuit.parameters[circuit.parameter_indices]))
+    return deflatten_parameters(circuit.circuit,OptParams), ObjFunc(OptParams)
 end
 
 function generate_offspring(population)
@@ -114,7 +122,7 @@ end
 function evaluate_fitness(population,measurements,frequencies)
     fitnesses = Array{Float32}(undef, length(population))
     for (e,individual) in enumerate(population)
-        OptParams, objective = CircuitFitness(individual,measurements,frequencies)
+        OptParams, objective = CircuitFitness_inclusive(individual,measurements,frequencies)
         individual.parameters[individual.parameter_indices] = OptParams
         fitnesses[e] = objective
     end
@@ -128,4 +136,32 @@ function TruncationSelection(population,offspring,population_fitness,offspring_f
     Next_generation = Total_population[Selected]
     Next_generation_fitness = Total_fitness[Selected]
     return Next_generation,Next_generation_fitness
+end
+
+function multi_point_mutation(circuit,N)
+    circuit = LRC_Circuit(circuit.karva,circuit.circuit,circuit.parameters,circuit.parameter_indices)
+    for i in 1:N
+        circuit = mutate(circuit)
+    end
+    return circuit
+end
+
+function segment_insertion(circuit)
+    circuit = LRC_Circuit(circuit.karva,circuit.circuit,circuit.parameters,circuit.parameter_indices)
+    karva = circuit.karva
+    parameters = circuit.parameters
+    total_length = length(karva)
+    head_length = (total_length-1)/2
+    tail_length = head_length+1
+    operations = "+-"
+    possible_insertions = 2:head_length
+    possible_starts = 1:total_length
+    start = rand(possible_starts)
+    instertion = rand(possible_insertions)
+    length_of_instertion = rand(1:(head_length-instertion))
+    insertion_karva = circuit.karva[start:start+length_of_instertion]
+    instertion_parameters = circuit.parameters[start:start+length_of_instertion]
+    circuit.karva[instertion:instertion+length_of_instertion] = insertion_karva
+    circuit.parameters[instertion:instertion+length_of_instertion] = instertion_parameters
+    return circuit
 end
--- src/InitializeParameters.jl
+++ src/InitializeParameters.jl
@@ -4,20 +4,29 @@ Provides a random set of parameters for a given circuit. Upper bounds of the
 parameters can be imposed with the range argument.
 
 """
-function InitializeParameters(Circuit,ranges=Dict('R'=>1000,'C'=>0.001,'L'=>1))
-    Elements = [Circuit[r] for r in findall(x->x=='R'||x=='C'||x=='L', Circuit)]
-    Element_values = Array{Float64}(undef, length(Elements))
+
+function InitializeParameters_inclusive(Circuit,ranges=Dict('R'=>1000,'C'=>0.001,'L'=>1,'P'=>[100,1]))
+    Elements = [Circuit[r] for r in findall(x->x=='R'||x=='C'||x=='L'||x=='P', Circuit)]
+    Element_values = Array{Any}(undef, length(Elements))
     for (n,e) in enumerate(Elements)
-            Element_values[n] = ranges[e]*rand()
+            if e == 'P'
+                Element_values[n] = [ranges[e][1]*rand(),ranges[e][2]*rand()]
+            else
+                Element_values[n] = ranges[e]*rand()
+            end
     end
     return Element_values
 end
 
-function karva_parameters(karva)
-    parameters = Array{Float16}(undef, length(karva))
-    ranges = Dict('R'=>1000,'C'=> 0.001,'L'=> 1,'+'=> 0,'-'=> 0)
+function karva_parameters_inclusive(karva)
+    parameters = Array{Any}(undef, length(karva))
+    ranges = Dict('R'=>1000,'C'=> 0.001,'L'=> 1,'+'=> 0,'-'=> 0,'P'=>[100,1])
     for (e,i) in enumerate(karva)
-        parameters[e] = rand()*ranges[i]
+            if e == 'P'
+                Element_values[n] = [ranges[e][1]*rand(),ranges[e][2]*rand()]
+            else
+            parameters[e] = rand()*ranges[i]
+        end
     end
     return parameters
 end
