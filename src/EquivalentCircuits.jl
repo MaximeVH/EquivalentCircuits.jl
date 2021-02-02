aba9bbc4853ea808a1d320ddc9e34c94a7c8915a
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2021-02-03 00:53:24 +0100

    add dependencies

--- src/EquivalentCircuits.jl
+++ src/EquivalentCircuits.jl
@@ -1,21 +1,17 @@
 module EquivalentCircuits
+    export circuitevolution
+    export parameteroptimisation
+    using Random, Combinatorics, FunctionWrappers, DelimitedFiles, Distributions, Optim, Plots
+    import Base: isless, length
 
-using Random, Combinatorics, FunctionWrappers, DelimitedFiles, Distributions, Optim, Plots
-import Base: isless
-import Base: length
-
-include("Circuits.jl") 
-include("CircuitFunction.jl")
-include("EvolutionOperators.jl")
-include("ObjectiveFunction.jl")
-include("OptimizeParameters.jl")
-include("SimulateImpedance.jl")
-include("CircuitSimplification.jl")
-include("CircuitLibrary.jl")
-include("RedundancyTesting.jl")
-include("CircuitEvolution.jl")
-
-
-export circuitevolution
-
+    include("Circuits.jl") 
+    include("CircuitFunction.jl")
+    include("EvolutionOperators.jl")
+    include("ObjectiveFunction.jl")
+    include("OptimizeParameters.jl")
+    include("SimulateImpedance.jl")
+    include("CircuitSimplification.jl")
+    include("CircuitLibrary.jl")
+    include("RedundancyTesting.jl")
+    include("CircuitEvolution.jl")
 end
