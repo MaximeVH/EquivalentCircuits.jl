fa50bbbc6a53e8ecf126c7058b63287eb720aed3
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2021-02-11 14:28:15 +0100

    new objective function.

--- src/ObjectiveFunction.jl
+++ src/ObjectiveFunction.jl
@@ -5,12 +5,38 @@ impedance for a given set of circuit parameters at a specified range of experime
 This function is used to optimize the circuit parameters.
 
 """
-function objectivefunction(circuitfunc,measurements,frequencies)
+function objectivefunction(circuitfunc,measurements,frequencies) #fitness measure from Ramos et al.
     function objective(x)
-        N = length(measurements)
+        model_output = [circuitfunc(x,fr) for fr in frequencies] 
+        return  mean((abs.(measurements - model_output).^2)./(abs.(model_output).^2))
+    end
+    return objective
+end
+
+#Some alternate objective functions.
+
+function objectivefunction2(circuitfunc,measurements,frequencies)
+    function objective(x)
         model_output = [circuitfunc(x,fr) for fr in frequencies] 
         rme,ime,rmo,imo = real(measurements),imag(measurements),real(model_output),imag(model_output)
         return sum((rme.-rmo).^2 .+ (ime.-imo).^2) 
     end
     return objective
 end
+
+function objectivefunction3(circuitfunc,measurements,frequencies) #fitness measure from Ramos et al.
+    function objective(x)
+        model_output = [circuitfunc(x,fr) for fr in frequencies] 
+        return  mean((abs.(measurements - model_output).^2)./(abs.(measurements).^2))
+    end
+    return objective
+end
+
+function objectivefunction4(circuitfunc,measurements,frequencies) #fitness measure from Ramos et al.
+    function objective(x)
+        model_output = [circuitfunc(x,fr) for fr in frequencies] 
+        return  mean((abs.(measurements - model_output).^2))
+    end
+    return objective
+end
+
