002347a03ff043a6df383eb8e9b8a7c6f9acfbeb
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2021-06-10 08:55:04 +0200

    Add function test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,16 @@
-#To be supplemented.
+using EquivalentCircuits
+using Test
+
+data = readdlm(raw"C:\Users\Dell\Documents\EquivalentCircuits.jl\example_measurements.csv",',')
+measurements = data[:,1] .+ data[:,2]im
+frequencies = data[:,3]
+library = loadpopulation("Circuitlibrary.csv");
+library_fit =["R1-[C2,R3-[C4,R5]]"
+"P1-L2-[R3,C4]-[R5,C6]"
+"[R1,C2-[R3,C4]]"
+"[R1-[P2,R3],C4]"
+"R1-[C2,R3-P4]"]
+
+@testset "EquivalentCircuits.jl" begin
+    library_fit == circuitevolution("example_measurements.csv",initial_population = library)
+end
