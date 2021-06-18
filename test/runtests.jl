7c23f6ad9b7daf43822ff4b2401c1a4c8351a923
MaximeVH <59018061+MaximeVH@users.noreply.github.com>
2021-06-18 15:40:50 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ data = readdlm(raw"C:\Users\Dell\Documents\EquivalentCircuits.jl\example_measure
 measurements = data[:,1] .+ data[:,2]im
 frequencies = data[:,3]
 library = loadpopulation("Circuitlibrary.csv");
-library_fit =["R1-[C2,R3-[C4,R5]]"
+library_fit ="[R1-[C2,R3-[C4,R5]]"
 "P1-L2-[R3,C4]-[R5,C6]"
 "[R1,C2-[R3,C4]]"
 "[R1-[P2,R3],C4]"
