babe50749966c9eab19261efd85d8cb0336ec5a2
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2021-06-25 19:31:37 +0200

    remove unnecessary things

--- src/EquivalentCircuits.jl
+++ src/EquivalentCircuits.jl
@@ -3,7 +3,7 @@ module EquivalentCircuits
     export circuitevolution
     export parameteroptimisation
     export loadpopulation
-    export initialize_circuitlibrary, add_to_circuitlibrary, add_encoding_to_circuitlibrary
+    export Circuit, EquivalentCircuit
     using Random, Combinatorics, GeneralizedGenerated, DelimitedFiles, Distributions, Optim
     import Base: isless, length
 
