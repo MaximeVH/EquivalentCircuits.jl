d940445e86950729dd2c848087aae8a6f74034d5
WC1991 <s51079@edu.campus02.at>
2022-11-28 14:24:42 +0100

    1.) circuitfunction: counting of variable "counter" corrected 2.) circuitfunction: formula for 'P' corrected 3.) tree_to_function: formula for 'P' corrected

--- src/CircuitFunction.jl
+++ src/CircuitFunction.jl
@@ -1,81 +1,51 @@
 function circuitfunction(Circuit)
-    b_new = true
-    b_debug = true
-    if b_debug println("Circuit, in: ", Circuit) end
-    for (f, t) in zip(["-", "[",  ",",      "]"],
-                      ["+", "((", ")^-1+(", ")^-1)^-1"])
-        Circuit= replace(Circuit, f=>t)
+    for (f, t) in zip(["-",  "[",    ","         ,"]"        ],
+                      ["+",  "((",   ")^-1+("    ,")^-1)^-1" ])
+        Circuit=replace(Circuit, f=>t)
     end
-    if b_debug println("Circuit, after replace: ", Circuit) end
     for I in 2:-1:1
         Es = eachmatch(Regex("([CLRPW])([0-9]){$(I)}"),Circuit)
             for e in Es
                 match = e.match
                 if match[1] == 'C'
-                    Circuit = replace(Circuit,match=>"(1/(2im*π*f*"*"T"*"))")
-                    if b_debug println("circuitfunction, Circuit 'C': ", Circuit) end
+                 Circuit = replace(Circuit,match=>"(1/(2im*π*f*"*"T"*"))")
                 elseif match[1] == 'R'
-                    Circuit = replace(Circuit,match=>"T")
+                 Circuit = replace(Circuit,match=>"T")
                 elseif match[1] == 'L'
-                    Circuit = replace(Circuit,match=>"(2im*π*f*"*"T"*")")
-                elseif match[1] == 'P'
-                    if b_debug println("P: Circuit; in: ", Circuit, ", match: ", match) end
-                    Circuit_P_new  = replace(Circuit,match=>"(1/(T*(2im*π*f)^N))")
-                    Circuit_P_orig = replace(Circuit,match=>"T*(2*π*f)^(-N)"*"*(cos((π*N)*0.5)-sin((π*T)*0.5)im)")
-                    if b_new
-                        Circuit = Circuit_P_new
-                        if b_debug println("new:  circuit") end
-                    else
-                    # original:
-                        Circuit = Circuit_P_orig
-                        if b_debug println("orig: circuit") end
-                    end
-                    if b_debug println("circuitfunction, Circuit 'P': ", Circuit_P_orig) end
-                    if b_debug println("circuitfunction, new     'P': ", Circuit_P_new) end
-                elseif match[1] == 'W'
-                    Circuit = replace(Circuit,match=>"T*(2*π*f)^(-0.5)"*"*(cos((π*0.5)*0.5)-sin((π*0.5)*0.5)im)")
+                 Circuit = replace(Circuit,match=>"(2im*π*f*"*"T"*")")
+             elseif match[1] == 'P'
+                 Circuit = replace(Circuit, match => "(1/("*"T"*"*(2im*π*f)^"*"N"*"))") # compatible to package "impedance.py"
+             elseif match[1] == 'W'
+                 Circuit = replace(Circuit,match=>"T*(2*π*f)^(-0.5)"*"*(cos((π*0.5)*0.5)-sin((π*0.5)*0.5)im)")
             end
         end
+end
+new_circuit = ""
+counter = 1
+for i in Circuit
+    if i == 'T'
+        new_circuit = new_circuit*"T["*string(counter)*"]"
+        counter += 1
+    elseif i == 'N'
+        new_circuit = new_circuit*"T["*string(counter)*"]"
+        counter += 1
+    else
+        new_circuit = new_circuit*i
     end
-    if b_debug println("circuit, final: \n", Circuit, "\n") end
-    # ---
-    new_circuit = ""
-    counter = 1
-    for i in Circuit
-        if i == 'T'
-            new_circuit = new_circuit*"T["*string(counter)*"]"
-            counter += 1
-        elseif i == 'N'
-            new_circuit = new_circuit*"T["*string(counter)*"]"
-            counter += 1
-        else
-            new_circuit = new_circuit*i
-        end
-    end
-    if b_debug println("new_circuit: ", new_circuit) end
+end
+
     Circuit_expression = Meta.parse(new_circuit)
-    if b_debug println("Circuit_expression: ", Circuit_expression) end
-    return mk_function([:T,:f],[], Circuit_expression)
+    return mk_function([:T,:f],[],Circuit_expression)
 end
 
 function tree_to_function(tree_array)
-    b_new = true
-    dictionary_of_calculations_orig = Dict('R'=>"T",'C'=>"(1/(2im*π*f*"*"T"*"))",'L'=>"(2im*π*f*"*"T"*")",'P'=>"T[1]*(2*π*f)^(-T[2])"*"*(cos((π*T[2])*0.5)-sin((π*T[2])*0.5)im)") 
-    dictionary_of_calculations_new = Dict('R'=>"T",'C'=>"(1/(2im*π*f*"*"T"*"))",'L'=>"(2im*π*f*"*"T"*")",'P'=>"(1/(T[1]*(2im*π*f)^T[2]))") 
-
-    if b_new
-        dictionary_of_calculations = dictionary_of_calculations_new
-        println("orig: ", dictionary_of_calculations_orig)
-        println("new:  ", dictionary_of_calculations_new)
-    else
-        dictionary_of_calculations = dictionary_of_calculations_orig
-    end
+    dictionary_of_calculations = Dict('R'=>"T",'C'=>"(1/(2im*π*f*"*"T"*"))",'L'=>"(2im*π*f*"*"T"*")",'P'=>"(1/(T[1]*(2im*π*f)^T[2]))") 
     tree = copy(tree_array) 
-    nodecount = length(tree)
+    # nodecount = length(tree) # curently not in use
     essential_info = [[node.ParentIndex,node.Type,node.Index] for node in tree]
     for i in 1:((length(essential_info)-1)/2)
-        parent1,type1,index1 = pop!(essential_info)
-        parent2,type2,index2 = pop!(essential_info)
+        parent1, type1, index1 = pop!(essential_info)
+        parent2, type2, index2 = pop!(essential_info) # parent2 currently not in use
         type1 = haskey(dictionary_of_calculations,type1) ? replace(dictionary_of_calculations[type1],"T"=>"T["*string(index1)*']') : type1
         type2 = haskey(dictionary_of_calculations,type2) ? replace(dictionary_of_calculations[type2],"T"=>"T["*string(index2)*']') : type2
         operation = essential_info[parent1][2]
