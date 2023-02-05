dd350bd96517beb5ee9f76fc402e0a0040098dd0
MaximeVH <maxime.vanhaeverbeke@gmail.com>
2023-02-05 08:31:32 +0100

    optional custom parameter bounds argument

--- src/OptimizeParameters.jl
+++ src/OptimizeParameters.jl
@@ -9,6 +9,12 @@ function get_parameter_upper_bound(readablecircuit::String)
     return flatten([ranges[e] for e in elements])
 end
 
+function get_parameter_upper_bound(readablecircuit::String, ranges = ::Dict)
+    ranges['+'] = 0; ranges['-'] = 0;
+    elements = foldl(replace,["["=>"","]"=>"","-"=>"",","=>""],init = denumber_circuit(readablecircuit))
+    return flatten([ranges[e] for e in elements])
+end
+
 function func_and_params_for_optim(tree) 
     circuit,circuit_parameters,param_inds = tree_to_circuit_with_inds(tree)
     circuitfunc = circuitfunction(circuit)
@@ -29,11 +35,12 @@ end
 Fit the parameters of a given equivalent circuit to measurement values, using the Nelder-Mead simplex algorithm.
 
 The inputs are the string representation of a circuit (e.g. "R1-[C2,R3]-P4"), an array of complex-valued impedance measurements and their corresponding frequencies.
-The output is NamedTuple of the circuit's components with their corresponding parameter values. Three optional keyword arguments are:
+The output is NamedTuple of the circuit's components with their corresponding parameter values. Four optional keyword arguments are:
 
 - `x0`: An optional initial parameterset
 - `weights`: A vector of equal length as the frequencies. This can be used to attatch more importance to specific areas within the frequency range.
 - `fixed_params`: A tuple with the indices of the parameters that are to be fixed during the optimisation and the corresponding fixed parameter values.
+- `param_ranges`: A Dictionary with the circuit components as keys the upperbounds of their respective parameter values as values.
 
 # Example
 ```julia
@@ -50,7 +57,7 @@ julia> frequencies = [0.10, 0.43, 1.83, 7.85, 33.60, 143.84, 615.85,  2636.65, 1
 julia> parameteroptimisation("R1-[C2,R3-[C4,R5]]",measurements,frequencies)
 (R1 = 19.953805651358255, C2 = 3.999778355811269e-9, R3 = 3400.0089192843684, C4 = 3.999911415903211e-6, R5 = 2495.2493215522577)
 """
-function parameteroptimisation(circuitstring::String,measurements,frequencies;x0=nothing,weights = nothing, fixed_params = nothing)
+function parameteroptimisation(circuitstring::String,measurements,frequencies;x0=nothing,weights = nothing, fixed_params = nothing, param_ranges = nothing)
     elements = foldl(replace,["["=>"","]"=>"","-"=>"",","=>""],init = denumber_circuit(circuitstring))
     initial_parameters = flatten(karva_parameters(elements));
     if isnothing(fixed_params)
@@ -60,7 +67,7 @@ function parameteroptimisation(circuitstring::String,measurements,frequencies;x0
     end
     objective = objectivefunction(circfunc,measurements,frequencies,weights) 
     lower = zeros(length(initial_parameters))
-    upper = get_parameter_upper_bound(circuitstring)
+    upper = isnothing(param_ranges) ? get_parameter_upper_bound(circuitstring) : get_parameter_upper_bound(circuitstring, param_ranges)
 
     ### First step ###
     SR = Array{Tuple{Float64,Float64},1}(undef,length(initial_parameters))
@@ -95,7 +102,7 @@ The inputs are the string representation of a circuit (e.g. "R1-[C2,R3]-P4") and
 the real part of the impedance, the imaginary part of the impedance, and the frequencies corresponding to the measurements.
 The output is NamedTuple of the circuit's components with their corresponding parameter values.
 """
-function parameteroptimisation(circuitstring::String,data::String;weights = nothing, fixed_params = nothing) 
+function parameteroptimisation(circuitstring::String,data::String;weights = nothing, fixed_params = nothing, param_ranges = nothing) 
     meansurement_file = readdlm(data,',')
     # convert the measurement data into usable format.
     reals = meansurement_file[:,1]
@@ -112,7 +119,7 @@ function parameteroptimisation(circuitstring::String,data::String;weights = noth
     end
     objective = objectivefunction(circfunc,measurements,frequencies,weights) 
     lower = zeros(length(initial_parameters))
-    upper = get_parameter_upper_bound(circuitstring)
+    upper = isnothing(param_ranges) ? get_parameter_upper_bound(circuitstring) : get_parameter_upper_bound(circuitstring, param_ranges)
     ### First step ###
     SR = Array{Tuple{Float64,Float64},1}(undef,length(initial_parameters))
     for (e,(l,u)) in enumerate(zip(lower,upper))
