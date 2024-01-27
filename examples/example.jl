c488e8bddce8d38c9a9a3a42bab4d3c2b901003d
Amin Sadeghi <amin.sadeghi@live.com>
2024-01-27 02:56:30 -0500

    Update example.jl script based on most recent API

--- examples/example.jl
+++ examples/example.jl
@@ -1,23 +1,30 @@
-using EquivalentCircuits, CSV
+using CSV
+using DataFrames
+using EquivalentCircuits
 
 """
-Circuitidentification is done with the circuitevolution function. This function can take several keyword arguments,
-allowing users to tune the circuitidentification if necessary. These can be found in the function documentation.
-The measurements and frequencies should be provided either as a CSV file, or as arrays.
+Circuit identification is done with the `circuit_evolution` function. This function can take
+several keyword arguments, allowing users to tune the circuit identification if necessary.
+These can be found in the function documentation. The measurements and frequencies should
+be provided either as a CSV file, or as arrays.
+"""
+# Example 1: Input is CSV file with as columns: Re(Z), Im(Z), frequencies measurements
+circuit = circuit_evolution("example_measurements.csv")
+
+# Example 2: Input is an array with as columns: Re(Z), Im(Z), frequencies measurements
+data = CSV.read("example_measurements.csv", DataFrame)
+Z = data[:, 1] .+ data[:, 2]im
+freq = data[:, 3]
+@time circuit = circuit_evolution(Z, freq)
 
 """
-#Measurements input as CSV file with as columns: real impedances, imaginary impedances and frequencies.
-circuitevolution("example_measurements.csv")
-
-#Example of input as arrays.
-data = readdlm(raw"C:\Users\Dell\Documents\EquivalentCircuits.jl\example_measurements.csv",',')
-measurements = data[:,1] .+ data[:,2]im
-frequencies = data[:,3]
-circuitevolution(measurements,frequencies)
+When the equivalent circuit to be used is known, its parameters can be fit using the
+`parameteroptimisation` function.
+"""
+p = parameteroptimisation("[[C1,R2]-R3,P4]", Z, freq)
 
 """
-When the equivalent circuit to be used is known, its parameters can be fit using the parameteroptimisation function.
-
+If you want to generate multiple candidate circuits, you can use the `circuit_evolution_batch`
+function. This function is parallelised on all available cores.
 """
-
-parameteroptimisation("[[C1,R2]-R3,P4]", measurements, frequencies)
+circuits = circuit_evolution_batch(Z, freq; iters=6);
